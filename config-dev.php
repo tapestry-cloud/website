771fbadf15403dedf2ab3834d36cb67fc68f8f76
Simon Dann <simon.dann@gmail.com>
2017-03-22 16:04:48 +0000

    :note: added documentation on project directory structure

---
+++ config-dev.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    /**
+     * Some example global data, this is available from $this->site(...) in any phtml file
+     */
+    'site' => [
+        'title' => 'dev.tapestry.cloud',
+        'url' => 'http://dev.tapestry.cloud'
+    ],
+];
