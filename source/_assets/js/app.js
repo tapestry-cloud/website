98a33b1abd6a9c6db187bd88a22362b7deeb4271
Simon Dann <simon.dann@gmail.com>
2017-03-22 16:04:48 +0000

    :note: added documentation on project directory structure

--- source/_assets/js/app.js
+++ source/_assets/js/app.js
@@ -4,5 +4,4 @@ import 'prismjs/components/prism-javascript.min.js';
 import 'prismjs/components/prism-bash.min.js';
 import 'prismjs/components/prism-php.min.js';
 import 'prismjs/components/prism-markdown.min.js';
-// import 'prism-treeview/prism-treeview.js';
-Prism.highlightAll();
+import 'prism-treeview/prism-treeview.js';
