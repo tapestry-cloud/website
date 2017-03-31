c35ca789445df7cf3341837e0dc0717400e9b524
Simon Dann <carbontwelve@users.noreply.github.com>
2017-03-31 11:53:48 +0100

    :sparkles: added a code example helper function

---
+++ lib/CodeExamplePlatesExtension.php
@@ -0,0 +1,22 @@
+<?php namespace TapestryCloud\Lib;
+
+use League\Plates\Engine;
+use League\Plates\Extension\ExtensionInterface;
+
+class CodeExamplePlatesExtension implements ExtensionInterface
+{
+    public function register(Engine $engine)
+    {
+        $engine->registerFunction('codeExample', [$this, 'codeExample']);
+    }
+
+    public function codeExample($file)
+    {
+        $filePath = __DIR__ . '/../source/_code-examples/' . $file;
+        if (! file_exists($filePath)) {
+            throw new \Exception('File ['. $filePath .'] does not exist.');
+        }
+
+        return htmlentities(file_get_contents($filePath));
+    }
+}
