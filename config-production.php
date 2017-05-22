6b6e7ea48e9d40e9bba70c62640deafbc2f16be1
Simon Dann <carbontwelve@users.noreply.github.com>
2017-05-22 16:17:50 +0100

    :memo: adding google analytics

---
+++ config-production.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    /**
+     * Some example global data, this is available from $this->site(...) in any phtml file
+     */
+    'site' => [
+        'title' => 'tapestry.cloud',
+        'url' => 'https://www.tapestry.cloud',
+        'google-analytics' => 'UA-55407-5'
+    ],
+];
