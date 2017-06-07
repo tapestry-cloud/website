4d5bfccef9fbcd18eac8a223a93bfc2a61b1ffce
Simon Dann <carbontwelve@users.noreply.github.com>
2017-06-07 15:41:14 +0100

    :wrench: adding menu to configuration

--- config.php
+++ config.php
@@ -10,7 +10,47 @@ return [
         'description' => 'Tapestry static site generator.',
         'author' => 'Simon Dann',
         'email' => 'simon.dann@gmail.com',
-        'google-analytics' => null
+        'google-analytics' => null,
+        'documentation-menu' => [
+
+            'Getting Started' => [
+                'Quickstart Guide' => 'documentation',
+                'Installation/Upgrade' => 'documentation/installation',
+                'Starting a new site' => 'documentation/start-new-website',
+                'Configuration' => 'documentation/configuration',
+                'Working Examples &amp; Plugins' => 'documentation/working-examples'
+            ],
+
+            'Your Content' => [
+                'Front Matter' => 'documentation/front-matter',
+                'Site Variables' => 'documentation/site-variables',
+                'Helpers' => 'documentation/helpers',
+                'Creating Content' => 'documentation/your-content',
+                // 'Markdown Extra' => 'documentation/markdown-extra',
+                // 'PHP Plates' => 'documentation/php-plates',
+                'Collections' => 'documentation/collections',
+                'Assets' => 'documentation/assets',
+                'Pretty URLS' => 'documentation/permalinks',
+                'Deploying your site' => 'documentation/deploying',
+            ],
+
+            'Core Concepts' => [
+                'Commands' => 'documentation/commands',
+                'Project Kernel' => 'documentation/kernel',
+                'Content Types' => 'documentation/content-types',
+                'Content Renderers' => 'documentation/content-renderers',
+                'Content Generators' => 'documentation/content-generators',
+                'Taxonomy' => 'documentation/taxonomy',
+            ],
+
+            'Extending Tapestry' => [
+                'Writing Plugins' => 'documentation/writing-plugins',
+                'Steps' => 'documentation/steps',
+                'Container' => 'documentation/container',
+                'Project' => 'documentation/project',
+                'Events' => 'documentation/events',
+            ]
+        ]
     ],
 
     'plugins' => [
