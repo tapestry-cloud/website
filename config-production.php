7286e86291c2119101fa2c69aad54694383f76e0
Simon Dann <carbontwelve@users.noreply.github.com>
2017-06-10 10:34:12 +0100

    :bug: removed production title overloading

--- config-production.php
+++ config-production.php
@@ -5,7 +5,6 @@ return [
      * Some example global data, this is available from $this->site(...) in any phtml file
      */
     'site' => [
-        'title' => 'tapestry.cloud',
         'url' => 'https://www.tapestry.cloud',
         'google-analytics' => 'UA-55407-5'
     ],
